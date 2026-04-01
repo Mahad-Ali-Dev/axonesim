d8fbf4ee2cabc4303fef4907d14edf58e2628112
Mahad Ali <mahad107600@gmail.com>
2026-04-01 14:36:29 +0500

    Update robots.txt and next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'api.qrserver.com' },
+
+
+      { protocol: 'https', hostname: 'axonesim.com' },
+      { protocol: 'https', hostname: 'www.axonesim.com' },
+
+      { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
   },
   serverExternalPackages: ['twilio'],
